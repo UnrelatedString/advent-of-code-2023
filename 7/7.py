3d3be83df3d5e31dc30f1a3bafaa6fdc2e6ba1bb
UnrelatedString <unrelatedstring@gmail.com>
2023-12-07 01:00:38 -0500

    whoever imagined sisyphus happy was fucking wrong

--- 7/7.py
+++ 7/7.py
@@ -13,21 +13,44 @@ def main():
     for line in slorp():
         h, b = line.split()
         b = int(b)
-        p = [
-            len(set(h)) == 1,
-            len(set(h)) == 2 and any(h.count(c) == 1 for c in h),
-            len(set(h)) == 2,
-            any(h.count(c) == 3 for c in h),
-            len(set(h)) == 3,
-            len(set(h)) == 4,
-            True
-        ].index(True)
-        hs.append([p, tuple(map(r, h)), b])
+        
+        # five of a kind
+        if any(h.count(c) == 5 for c in h):
+            assert len(set(h)) == 1
+            p = 0
+        # four of a kind
+        elif any(h.count(c) == 4 for c in h) and any(h.count(c) == 1 for c in h):
+            assert len(set(h)) == 2
+            p = 1
+        # full house
+        elif any(h.count(c) == 3 for c in h) and any(h.count(c) == 2 for c in h):
+            assert len(set(h)) == 2
+            p = 2
+        # three of a kind
+        elif any(h.count(c) == 3 for c in h):
+            assert len(set(h)) == 3
+            p = 3
+        # two pair
+        elif any(c != d and h.count(c) == 2 and h.count(d) == 2 for c in h for d in h):
+            assert len(set(h)) == 3
+            p = 4
+        # one pair
+        elif any(h.count(c) == 2 for c in h):
+            assert len(set(h)) == 4
+            p = 5
+        # high card
+        else:
+            assert len(set(h)) == 5
+            p = 6
+        
+        hs.append([-p, *(-r(c) for c in h), b])
     
-    hs.sort()
+    # hs = hs[::-1]
+    hs = sorted(hs, key = lambda l: l[:-1])
+    # print(*hs)
     s = 0
-    for i, x in enumerate(hs):
-        s += -~i * x[2]
+    for i, x in enumerate(hs[::-1]):
+        s += (i + 1) * x[-1]
     print(s)
 
 if __name__ == '__main__':
