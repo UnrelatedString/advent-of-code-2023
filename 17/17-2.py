45167d5658acb4629cb2ecc866e7739ae3233737
UnrelatedString <unrelatedstring@gmail.com>
2023-12-17 01:17:04 -0500

    1271/1419
    next up: make a fucking priority queue for next time so it doesn't take 5 minutes to test
    or even just wrap heapq in a less hideous api
    maybe write a full on prepackaged dijkstra's that just takes functions for the stuff that changes

---
+++ 17/17-2.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+# from ..septic_tank import * # why can't we have good things
+exec(open('../septic_tank.py').read())
+
+
+# so I don't have any more accidentally global internal loop variables...
+def main():
+    losses = [[int(n) for n in r] for r in slorp()]
+    w, h = len(losses[0]), len(losses)
+    dest = complex(~-w, ~-h)
+    dests = {(dest, s, d) for s in range(3, 11) for d in von_neumann}
+
+    start = 0, 0, 0
+    unvisited = {start}
+    visited = set()
+    ds = defaultdict(lambda: float('inf'))
+    ds[start] = 0
+
+    while not visited & dests:
+        current = min(unvisited, key = lambda n: ds[n])
+        unvisited.remove(current)
+        visited.add(current)
+        for o in von_neumann:
+            cp, cs, cd = current
+            z = cp + o
+            s = -~cs if o == cd else 0
+            if not (0 <= z.real < w and 0 <= z.imag < h):
+                continue
+            if s >= 10:
+                continue
+            if o == -cd:
+                continue
+            if cd and o != cd and cs <= 2:
+                continue
+            if (z, s, o) in visited:
+                continue
+            ds[z, s, o] = min(ds[z, s, o], ds[current] + zind(z, losses))
+            unvisited.add((z, s, o))
+        # print(unvisited)
+    # print(ds)
+    print(min((ds[d]) for d in dests))
+    
+
+
+if __name__ == '__main__':
+    main()
